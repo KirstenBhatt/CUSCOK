36dadd871fc225162a49924ac4906c3c2e2feac7
jncutter <175655267+jncutter@users.noreply.github.com>
2025-02-06 15:35:35 -0600

    Add toggle to tab index and aria hidden values
    Improves accessibility while maintaining animation. The menu will
    be hidden from screen readers when closed, and unhidden when open.

--- script.js/menu.js
+++ script.js/menu.js
@@ -1,13 +1,40 @@
 const hamburger = document.getElementById('hamburger');
 const menu = document.getElementById('menu');
+const links = document.querySelectorAll('.menu-link')
 
 hamburger.addEventListener('click', () => {
   menu.classList.toggle('show');
+  menu.classList.toggle('hide');
+
+  if (menu.getAttribute('aria-hidden') == 'true') {
+    menu.setAttribute('aria-hidden', 'false');
+    links.forEach(element => {
+      element.setAttribute('tabindex', '0');
+    });
+  } else if (menu.getAttribute('aria-hidden') == 'false') {
+    menu.setAttribute('aria-hidden', 'true');
+    links.forEach(element => {
+      element.setAttribute('tabindex', '-1');
+    });
+  }
 });
 
 hamburger.addEventListener('keyup', (e) => {
   if (e.key === "Enter") {
-  menu.classList.toggle('show');
+    menu.classList.toggle('show');
+    menu.classList.toggle('hide');
+    
+    if (menu.getAttribute('aria-hidden') == 'true') {
+      menu.setAttribute('aria-hidden', 'false');
+      links.forEach(element => {
+        element.setAttribute('tabindex', '0');
+      });
+    } else if (menu.getAttribute('aria-hidden') == 'false') {
+      menu.setAttribute('aria-hidden', 'true');
+      links.forEach(element => {
+        element.setAttribute('tabindex', '-1');
+      });
+    }
   }
 });
 
@@ -15,5 +42,18 @@ hamburger.addEventListener('keyup', (e) => {
 window.addEventListener('click', (e) => {
   if (!hamburger.contains(e.target) && !menu.contains(e.target)) {
     menu.classList.remove('show');
+    menu.classList.add('hide');
+
+    if (menu.getAttribute('aria-hidden') == 'true') {
+      menu.setAttribute('aria-hidden', 'false');
+      links.forEach(element => {
+        element.setAttribute('tabindex', '0');
+      });
+    } else if (menu.getAttribute('aria-hidden') == 'false') {
+      menu.setAttribute('aria-hidden', 'true');
+      links.forEach(element => {
+        element.setAttribute('tabindex', '-1');
+      });
+    }
   }
 });
