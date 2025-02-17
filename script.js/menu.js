99cd2c46ed3f43a775d9c990bdb33315ad221ee6
jncutter <175655267+jncutter@users.noreply.github.com>
2025-02-17 16:12:00 -0600

    Simplify script by changing inert property on menu

--- script.js/menu.js
+++ script.js/menu.js
@@ -1,34 +1,28 @@
 const hamburger = document.getElementById('hamburger');
 const menu = document.getElementById('menu');
-const links = document.querySelectorAll('.menu-link')
 
-function toggleClasses() {
+function openCloseMenu() {
   menu.classList.toggle('show');
   menu.classList.toggle('hide');
 
-  if (menu.getAttribute('aria-hidden') == 'true') {
-    menu.setAttribute('aria-hidden', 'false');
-    links.forEach(element => {
-      element.setAttribute('tabindex', '0');
-    });
-  } else if (menu.getAttribute('aria-hidden') == 'false') {
-    menu.setAttribute('aria-hidden', 'true');
-    links.forEach(element => {
-      element.setAttribute('tabindex', '-1');
-    });
-  }
+  menu.inert = menu.inert ? false : true;
 }
 
-hamburger.addEventListener('click', toggleClasses);
+hamburger.addEventListener('click', openCloseMenu);
 
 hamburger.addEventListener('keyup', (e) => {
   if (e.key === "Enter") {
-    toggleClasses();
+    openCloseMenu();
   }
 });
 
 // Optional: Close the menu if clicked outside
-window.addEventListener('click', (e) => {
+//This is probably an unnecessary bit of code. The menu button should have
+//a transition that turns the hamburger into an 'x' so closing the menu will
+//be more intuitive. The code below makes it too easy to accidentally make the menu
+//unusable.
+
+/* window.addEventListener('click', (e) => {
   if (!hamburger.contains(e.target) && !menu.contains(e.target)) {
     menu.classList.remove('show');
     menu.classList.add('hide');
@@ -45,4 +39,4 @@ window.addEventListener('click', (e) => {
       });
     }
   }
-});
+}); */
