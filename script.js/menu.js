90bdcc9b8caa23b05eb495d9531d08955a88aa5d
jncutter <175655267+jncutter@users.noreply.github.com>
2025-02-20 09:58:03 -0600

    Remove commented code

--- script.js/menu.js
+++ script.js/menu.js
@@ -15,28 +15,3 @@ hamburger.addEventListener('keyup', (e) => {
     openCloseMenu();
   }
 });
-
-// Optional: Close the menu if clicked outside
-//This is probably an unnecessary bit of code. The menu button should have
-//a transition that turns the hamburger into an 'x' so closing the menu will
-//be more intuitive. The code below makes it too easy to accidentally make the menu
-//unusable.
-
-/* window.addEventListener('click', (e) => {
-  if (!hamburger.contains(e.target) && !menu.contains(e.target)) {
-    menu.classList.remove('show');
-    menu.classList.add('hide');
-
-    if (menu.getAttribute('aria-hidden') == 'true') {
-      menu.setAttribute('aria-hidden', 'false');
-      links.forEach(element => {
-        element.setAttribute('tabindex', '0');
-      });
-    } else if (menu.getAttribute('aria-hidden') == 'false') {
-      menu.setAttribute('aria-hidden', 'true');
-      links.forEach(element => {
-        element.setAttribute('tabindex', '-1');
-      });
-    }
-  }
-}); */
